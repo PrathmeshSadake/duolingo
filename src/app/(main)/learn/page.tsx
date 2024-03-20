0c1d318052673b93fba7b7d583f390eaf9853097
Prathmesh Sadake <prathmeshsadake@gmail.com>
2024-03-20 23:28:33 +0530

    Update layout and add components for LearnPage

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+import UserProgress from "@/components/user-progress";
+
+import Header from "./_components/header";
 
 const LearnPage = () => {
   return (
-    <div>LearnPage</div>
-  )
-}
+    <div className='flex flex-row-reverse gap-[48px] px-6'>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{
+            title: "Spanish",
+            imageSrc: "/assets/es.svg",
+          }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title='Spanish' />
+      </FeedWrapper>
+    </div>
+  );
+};
 
-export default LearnPage
+export default LearnPage;
